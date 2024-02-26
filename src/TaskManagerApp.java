08b7c49fd5bfa109c5dc88687d4c07bd24e1674b
aniuka6 <ania.tokarzewska@wp.pl>
2024-02-26 13:38:48 +0100

    first commit

---
+++ src/TaskManagerApp.java
@@ -0,0 +1,7 @@
+public class TaskManagerApp {
+    public static void main(String[] args) {
+        TaskManager taskManager = new TaskManager();
+        taskManager.mainLoop();
+       ;
+    }
+}
